5c1fc9520b50ac25453c166c26feba6f1913aac8
Kevin Sparks <kevin.sparks@yellow.co.nz>
2014-01-04 18:56:46 +1300

    Refs: #2 - Added authentication to API. Also created hard limits per session user for Items. (the requesting user may only see their own Items)

--- tracker/api.py
+++ tracker/api.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.conf.urls import url
 from tastypie import http
 from tastypie.resources import ModelResource
+from tastypie.authentication import ApiKeyAuthentication
 from tastypie.authorization import Authorization
 from tastypie.exceptions import ImmediateHttpResponse
+from tastypie.utils import trailing_slash
 from tracker.models import Item
-from django.contrib.auth.models import User
 
 
 class BaseResource(ModelResource):
@@ -13,34 +17,126 @@ class BaseResource(ModelResource):
         return 'application/json'
 
 
+# Tasypie authenticate code inspired by:
+# http://stackoverflow.com/questions/11770501/how-can-i-login-to-django-using-tastypie
 class UserResource(BaseResource):
 
     class Meta:
-        #authentication = ApiKeyAuthentication
-        list_allowed_methods = [] # Disallow all batch requests
-        detail_allowed_methods = ['get', 'put', 'delete']
         queryset = User.objects.all()
-        fields = ['username', 'first_name', 'last_name']
+        fields = ['first_name', 'last_name', 'username']
+        list_allowed_methods = [] # Disallow all batch requests
+        detail_allowed_methods = ['get', 'put', 'post']
         resource_name = 'user'
         authorization= Authorization()
 
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/authenticate%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('authenticate'), name="api_login"),
+            url(r"^(?P<resource_name>%s)/register%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('register'), name="api_register"),
+        ]
+
+    def authenticate(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+        data = self.deserialize(request, request.body,
+                                format=request.META.get('CONTENT_TYPE', 'application/json'))
+        username = data.get('username', '')
+        password = data.get('password', '')
+        # Test if the user credentials will authenticate
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                # Hooray, the user authenticates and is valid. Response
+                # with Api Key and some other useful fields
+                return self.create_response(request, {
+                    'success': True,
+                    'username': user.username,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'api_key': user.api_key.key,
+                    'hours': user.profile.hours,
+                })
+            else:
+                return self.create_response(request, {
+                    'success': False,
+                    'reason': 'Your account has been disabled',
+                    }, http.HttpForbidden)
+        else:
+            return self.create_response(request, {
+                'success': False,
+                'reason': 'Username or password is incorrect',
+                }, http.HttpUnauthorized)
+
+
+    @staticmethod
+    def _username_exists(username):
+        """ Quickly test if a username already exists """
+        try:
+            user = User.objects.get(username=username)
+            return True
+        except User.DoesNotExist:
+            return False
+
+    def register(self, request, **kwargs):
+        """ Creat a user object and return an API Key """
+        self.method_check(request, allowed=['post'])
+        data = self.deserialize(request, request.body,
+                                format=request.META.get('CONTENT_TYPE', 'application/json'))
+
+        valid = True
+        message = ""
+        if len(data.get('username', '')) == 0:
+            valid = False
+            message = "A username is required"
+        elif self._username_exists(data.get('username', '')):
+            valid = False
+            message = "That username has already been taken"
+        elif len(data.get('password', '')) == 0:
+            valid = False
+            message = "A password is required"
+
+        if valid:
+            # Passed the basic validation, not try create the user, which might
+            # raise further validation issues.
+            try:
+                user = User.objects.create_user(data.get('username'), data.get('email'),
+                                                data.get('password'))
+                user.first_name = data.get('first_name', "")
+                user.last_name = data.get('last_name', "")
+                user.save()
+            except Exception as e:
+                # Model entity validation
+                return self.create_response(request, {
+                    'success': False,
+                    'reason': e,
+                    }, http.HttpBadRequest)
+        else:
+            return self.create_response(request, {
+                'success': False,
+                'reason': message,
+                }, http.HttpBadRequest)
+
+        return self.authenticate(request, **kwargs)
+
 
 class ItemResource(BaseResource):
+    """ Expose the Item entities over REST, and provide a level of authorisation """
+    def obj_create(self, bundle, **kwargs):
+        """ Any "create" methods must use the session user always """
+        return super(ItemResource, self).obj_create(bundle, user=bundle.request.user)
 
-    def get_object_list(self, request):
-        print request
-        if request.user.is_authenticated():
-            userid = request.user.id
-            return super(ItemResource, self).get_object_list(request)\
-                                            .filter(user=userid)
-        else:
-            # Response with a 401 Unauthorised
-            raise ImmediateHttpResponse(response=http.HttpUnauthorized())
+    def apply_authorization_limits(self, request, object_list):
+        """ All "list" methods must filter by this user only """
+        return object_list.filter(user=request.user)
 
     class Meta:
-        #authentication = ApiKeyAuthentication
-        list_allowed_methods = ["get"]  # Don't allow batch deletes etc.
+        list_allowed_methods = ["get", "post"]
+        detail_allowed_methods = ['get', 'put', 'delete']
         queryset = Item.objects.all()
         resource_name = 'item'
-
+        authorization= Authorization()
+        authentication = ApiKeyAuthentication()
 
--- tracker/models.py
+++ tracker/models.py
@@ -1,10 +1,32 @@
+import hashlib
+import time
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.conf import settings
+from tastypie.models import ApiKey
 
 
-#TODO: make a signal to create a user profile, and an API Key on user.save()
+def make_api_key():
+    """ Uses timestamp and secret key salt """
+    timestamp = str(time.time())
+    return hashlib.sha1(timestamp + ":" + settings.SECRET_KEY).hexdigest()
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    """ Create an profile entity and API Key for this user """
+    if created:
+        profile, created = Profile.objects.get_or_create(user=instance)
+        apikey, created = ApiKey.objects.get_or_create(user=instance,
+                                                       key=make_api_key())
+
+
+# Connect the function to the signal
+post_save.connect(create_user_profile, sender=User)
+
 
 class Profile(models.Model):
+    """ One2One on user model used to store the preferred working hours field """
     user = models.OneToOneField(User, related_name="profile")
     hours = models.IntegerField(default=4, help_text="Preferred amount of working "
                                                      "hours per day")
@@ -26,7 +48,7 @@ class Item(models.Model):
 
     @property
     def undertime(self):
-        """Returns True when the the current duration is shorter than preferred"""
+        """ Returns True when the the current duration is shorter than preferred """
         return self.duration < self.user.profile.preferred_hours
 
 
